260505549c48c7470c3c3b1813c61942d88cc565
Marc Grunberg <marc.grunberg@unistra.fr>
2024-06-01 15:45:15 +0000

    force version

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+        name='seisbench',
+        version='0.7.0',
+        packages=find_packages(),
+
+)
